a6d45f3854c8603810d23401ba346635da6c4855
xiaoran <1209255038@qq.com>
2017-08-26 23:06:09 +0800

    创建高德Demo Git项目

--- app/src/main/java/com/wxr/android/activity/GaoDeBaseActivity.java
+++ app/src/main/java/com/wxr/android/activity/GaoDeBaseActivity.java
@@ -3,7 +3,7 @@ package com.wxr.android.activity;
 import android.support.v7.app.AppCompatActivity;
 
 /**
- * Created by Administrator on 2017/8/26.
+ * Created by ran on 2017/8/26.
  */
 
 public class GaoDeBaseActivity extends AppCompatActivity{
